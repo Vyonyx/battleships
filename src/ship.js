43fe0b77d123a7b5b7eabe0cd68582581e54a8d0
Shaneel Kumar <shaneel_kumar@live.com>
2022-06-25 02:22:48 +1200

    Created ship factory function (with tests).

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,21 @@
-function add(a, b) { return a + b }
+function ship(lengthID) {
 
-module.exports = add
+    const shipTypes = {
+        'carrier': 5,
+        'battleship': 4,
+        'cruiser': 3,
+        'destroyer': 2
+    }
+    const length = typeof lengthID == 'number' ? lengthID : shipTypes[lengthID]
+    let hitAmount = 0
+
+
+    return {
+        length,
+        hit: (x = 1) => hitAmount += x,
+        hitsTaken: () => hitAmount,
+        isSunken: () => hitAmount >= length ? true : false
+    }
+}
+
+module.exports = ship
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,5 +1,23 @@
-const add = require('./ship')
+const ship = require('./ship')
 
-test("simple addition.", () => {
-    expect(add(1, 2)).toBe(3)
+describe('Ship outputs', () => {
+    test('Obtain the length of a given ship.', () => {
+        expect(ship(5).length).toBe(5)
+        expect(ship('carrier').length).toBe(5)
+        expect(ship('destroyer').length).toBe(2)
+    })
+
+    test('Communicate how many times it has been hit.', () => {
+        const newShip = ship('destroyer')
+        newShip.hit(2)
+        expect(newShip.hitsTaken()).toBe(2)
+    })
+
+    test('Communicate if it has sunken yet.', () => {
+        const newShip = ship('carrier')
+        newShip.hit()
+        expect(newShip.isSunken()).toBe(false)
+        newShip.hit(4)
+        expect(newShip.isSunken()).toBe(true)
+    })
 })
