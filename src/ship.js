535a4d83b5ba23f40fe181feafa864ecd457b629
Shaneel Kumar <shaneel_kumar@live.com>
2022-06-25 22:15:04 +1200

    Ship FF & tests refactored to track segments hit.

--- src/ship.js
+++ src/ship.js
@@ -7,15 +7,29 @@ function ship(lengthID) {
         'destroyer': 2
     }
     const length = typeof lengthID == 'number' ? lengthID : shipTypes[lengthID]
-    let hitAmount = 0
+    let segments = Array(length).fill('safe')
 
 
     return {
         length,
-        hit: (x = 1) => hitAmount += x,
-        hitsTaken: () => hitAmount,
-        isSunken: () => hitAmount >= length ? true : false
+        hit: function(...arg) { [...arg].forEach(x => segments[x] = 'hit') },
+        hitsTaken: () => segments.filter(seg => seg === 'hit').length,
+        isSunken: () => segments.filter(seg => seg === 'hit').length >= length ? true : false,
+        getSegments: () => segments
     }
 }
 
-module.exports = ship
+function gameBoard(gridCells = 3) {
+    let positions = []
+    let gameGrid = Array(gridCells).fill(Array(gridCells).fill('x'))
+    let ships = []
+    let shipPositions = []
+
+    return {
+        gameGrid,
+        assignShipPosition: (shipType, x, y) => positions.push({ship: shipType, xPos: x, yPos: y}),
+        shipPositions: () => positions
+    }
+}
+
+module.exports = { ship, gameBoard }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,23 +1,59 @@
-const ship = require('./ship')
+const {ship, gameBoard} = require('./ship')
 
-describe('Ship outputs', () => {
+describe('Ship factory.', () => {
+
     test('Obtain the length of a given ship.', () => {
         expect(ship(5).length).toBe(5)
         expect(ship('carrier').length).toBe(5)
         expect(ship('destroyer').length).toBe(2)
     })
 
+    test('Register a hit on the ship.', () => {
+        const newShip = ship('destroyer')
+        newShip.hit(0)
+        console.log(newShip.getSegments())
+        expect(newShip.getSegments()).toEqual(['hit', 'safe'])
+    })
+
     test('Communicate how many times it has been hit.', () => {
         const newShip = ship('destroyer')
-        newShip.hit(2)
+        newShip.hit(0, 1)
         expect(newShip.hitsTaken()).toBe(2)
     })
 
     test('Communicate if it has sunken yet.', () => {
         const newShip = ship('carrier')
-        newShip.hit()
+        newShip.hit(0)
         expect(newShip.isSunken()).toBe(false)
-        newShip.hit(4)
+        newShip.hit(1, 2, 3, 4)
         expect(newShip.isSunken()).toBe(true)
     })
+
+})
+
+
+
+describe('Gameboard inputs.', () => {
+
+    test('Create a nested array based on number of grids required.', () => {
+        const cellCount = 5
+        const expected = Array(cellCount).fill(Array(cellCount).fill('x'))
+        expect(gameBoard(cellCount).gameGrid).toEqual(expected)
+    })
+
+    test('Assign ship position based on coordinates.', () => {
+        const x = 0
+        const y = 0
+        const ship = 'carrier'
+
+        const newBoard = gameBoard()
+        newBoard.assignShipPosition(ship, x, y)
+        expect(newBoard.shipPositions()).toEqual([{ship: 'carrier', xPos: x, yPos: y}])
+    })
+
+    test.todo('Place ships at a specified location by calling ship factory function.')
+    test.todo('Receive attack function and determines if ship is hit and finally sends calls hit function on ship.')
+    test.todo('Keep track of missed attacks so that they can used for display.')
+    test.todo('Report whether all ships have been sunken.')
+
 })
